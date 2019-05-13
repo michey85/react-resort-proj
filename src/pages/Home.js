1a31fab20c95a25605902a217757fa4598ad95b0
Mikhail Nepomnyashchiy <nma@sessia.com>
2019-05-13 12:58:17 +0300

    header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
 	return (
+		<>
+		<Hero hero="defaultHero">
+			<Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+				<Link to="rooms" className="btn-primary">
+					Our rooms
+				</Link>
+			</Banner>
+		</Hero>
 		<div>Hello from Homepage</div>
+		</>
 	)
 };
 
